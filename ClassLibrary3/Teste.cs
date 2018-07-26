479453e651a48f06d8b0d146cecf56e442aba98e
wtof <wt.fox@hotmail.com>
2018-07-26 19:54:10 -0300

    teste commit e push

---
+++ ClassLibrary3/Teste.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PesRivais.teste
+
+{
+    [TestFixture]
+    public class Teste
+    {
+        [Test]
+        public void Main()
+        {
+            Assert.AreEqual(1,1);
+        }
+    }
+}
---
+++ PesRivaisTest/Teste.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace PesRivaisTest
+{
+    public class Teste
+
+    {
+    }
+}
